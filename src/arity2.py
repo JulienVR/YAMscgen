871a03fcdaff4e14b5865959294a157645396a4d
Julien Van Roy <juvr@odoo.com>
2024-05-03 16:36:43 +0200

    arrow to self working with a arcgradient=0 only

--- src/arity2.py
+++ src/arity2.py
@@ -27,15 +27,51 @@ class Arc(Arity2):
         x1 = builder.participants_coordinates[self.src]
         x2 = builder.participants_coordinates[self.dst]
         y2 = y1 + builder.parser.context['arcgradient']
-        ET.SubElement(root, 'line', {
-            **self.options,
-            'stroke': 'black',
-            'x1': str(x1),
-            'y1': str(y1),
-            'x2': str(x2),
-            'y2': str(y2),
-        })
-        # Triangle (example: <polygon fill="purple" points="450,215 440,221 440,209"/>)
+        if self.src == self.dst:
+            # Special case: curved arc
+            ET.SubElement(root, 'path', {
+                **self.options,
+                'stroke': 'black',
+                'd': f"M {x1} {y1} A200,15 0 0,1 {x1},{y1 + builder.vertical_step}",
+                'fill': 'none',
+            })
+            builder.current_height += builder.vertical_step
+            utils.expand_lifelines(builder, root, self.options)
+        else:
+            # Class line arc
+            ET.SubElement(root, 'line', {
+                **self.options,
+                'stroke': 'black',
+                'x1': str(x1),
+                'y1': str(y1),
+                'x2': str(x2),
+                'y2': str(y2),
+            })
+        # Label
+        label = self.options.get('label')
+        if label:
+            g = ET.Element('g')
+            x_mean = (x1 + x2)/2
+            y_mean = (y1 + y2)/2
+            # ET.SubElement(g, 'rect', {
+            #     # upper left corner coordinates
+            #     'x': str(x_mean),
+            #     'y': str(y_mean),
+            #     # length and height of the rectangle
+            #     'width': '40',
+            #     'height': '20',
+            #     'fill': 'grey',
+            # })  # TODO: draw rectangle behind the text elements
+            text = ET.SubElement(g, 'text',  {
+                'x': str(x1 + 5) if self.src == self.dst else str(x_mean),
+                'y': str(y_mean - 5),
+                'text-anchor': 'middle' if self.src != self.dst else '',  # the text will be centered around the given coordinates
+            })
+            text.text = label
+            root.append(g)
+        # Triangle
+        if self.src == self.dst:
+            y2 += builder.vertical_step
         y1, y3 = y2 + 6, y2 - 6
         if x1 < x2:
             x1 = x3 = x2 - 10
