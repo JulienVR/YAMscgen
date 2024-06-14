2cd38ed6f4e0a6d9bc593aba96210ddac329e080
Julien Van Roy <juvr@odoo.com>
2024-06-14 11:12:01 +0200

    refactor arrows and labels

--- src/arity2.py
+++ src/arity2.py
@@ -2,29 +2,6 @@ import xml.etree.ElementTree as ET
 
 from . import utils
 
-# TODO generify usage of font
-HELVETICA = {
-    'capheight': 718,
-    'xheight': 523,
-    'ascender': 718,
-    'descender': -207,
-    'unicode_to_width': {
-        32: 278, 33: 278, 34: 355, 35: 556, 36: 556, 37: 889, 38: 667, 39: 222, 40: 333, 41: 333, 42: 389, 43: 584,
-        44: 278, 45: 333, 46: 278, 47: 278, 48: 556, 49: 556, 50: 556, 51: 556, 52: 556, 53: 556, 54: 556, 55: 556,
-        56: 556, 57: 556, 58: 278, 59: 278, 60: 584, 61: 584, 62: 584, 63: 556, 64: 1015, 65: 667, 66: 667, 67: 722,
-        68: 722, 69: 667, 70: 611, 71: 778, 72: 722, 73: 278, 74: 500, 75: 667, 76: 556, 77: 833, 78: 722, 79: 778,
-        80: 667, 81: 778, 82: 722, 83: 667, 84: 611, 85: 722, 86: 667, 87: 944, 88: 667, 89: 667, 90: 611, 91: 278,
-        92: 278, 93: 278, 94: 469, 95: 556, 96: 222, 97: 556, 98: 556, 99: 500, 100: 556, 101: 556, 102: 278, 103: 556,
-        104: 556, 105: 222, 106: 222, 107: 500, 108: 222, 109: 833, 110: 556, 111: 556, 112: 556, 113: 556, 114: 333,
-        115: 500, 116: 278, 117: 556, 118: 500, 119: 722, 120: 500, 121: 500, 122: 500, 123: 334, 124: 260, 125: 334,
-        126: 584, 161: 333, 162: 556, 163: 556, 164: 167, 165: 556, 166: 556, 167: 556, 168: 556, 169: 191, 170: 333,
-        171: 556, 172: 333, 173: 333, 174: 500, 175: 500, 177: 556, 178: 556, 179: 556, 180: 278, 182: 537, 183: 350,
-        184: 222, 185: 333, 186: 333, 187: 556, 188: 1000, 189: 1000, 191: 611, 193: 333, 194: 333, 195: 333, 196: 333,
-        197: 333, 198: 333, 199: 333, 200: 333, 202: 333, 203: 333, 205: 333, 206: 333, 207: 333, 208: 1000, 225: 1000,
-        227: 370, 232: 556, 233: 778, 234: 1000, 235: 365, 241: 889, 245: 278, 248: 222, 249: 611, 250: 944, 251: 611,
-    }
-}
-
 
 class Arity2:
     def __init__(self, src, dst, element, options):
@@ -90,71 +67,11 @@ class Arc(Arity2):
                     'fill': color,
                 })
 
-    def draw_label(self, root, x1, x2, y1, y2, font_size):
-        label = self.options.get('label')
-        if not label:
-            return
-        MARGIN = 1  # margin before and after the label
-        OFFSET = HELVETICA['ascender']/2 * font_size/1000  # offset above the arc
-        g = ET.Element('g')
-        lines_count = len(label.split('\n'))
-        for idx, lab in enumerate(label.split('\n')):  # labels may contain newline character
-            # Draw Boxes
-            text_width = sum(HELVETICA['unicode_to_width'].get(ord(c), 0) for c in lab) * font_size / 1000
-            if self.src == self.dst:
-                x = x1 + OFFSET
-                y = (y1 - OFFSET)
-            else:
-                x = min(x1, x2) + abs(x2 - x1) / 2 - text_width / 2
-                y = ((y1 + y2) / 2 - OFFSET)
-            text_height = (HELVETICA['ascender'] - HELVETICA['descender']) * font_size / 1000
-            scaled_ascender = HELVETICA['ascender'] * font_size / 1000
-            y -= (lines_count - idx - 1) * text_height
-            box_x = x - MARGIN
-            if lab == '':
-                continue
-            rect = ET.SubElement(g, 'rect', {
-                'x': str(box_x),
-                'y': str(y - scaled_ascender),
-                'width': str(text_width + 2 * MARGIN),
-                'height': str(text_height),
-                'fill': self.options.get('textbgcolour') or self.options.get('textbgcolor') or 'white',
-            })
-            # Sometimes, the text goes beyond the SVG frame
-            max_x = float(rect.attrib['x']) + float(rect.attrib['width'])
-            if max_x > float(root.attrib['width']):
-                root.attrib['width'] = str(max_x)
-            # Draw text inside the box
-            text = ET.SubElement(g, 'text', {
-                'x': str(box_x),
-                'y': str(y),
-                'style': f"font-size: {font_size}",
-                'font-family': 'Helvetica',
-            })
-            text.text = lab
-        root.append(g)
-
-    def draw(self, builder, root: ET.Element):
-        # Arc color
-        color = self.options.get('linecolour') or self.options.get('linecolor') or "black"
-        arrow_id = self.get_arrow_tip_id(color)
-        # Arc coordinates
-        x1 = builder.participants_coordinates[self.src]
-        y1 = builder.current_height + builder.margin
-        x2 = builder.participants_coordinates[self.dst]
-        y2 = y1 + builder.parser.context['arcgradient']
-        # Label may have multiple lines, hence needing the arcs to be shifted downwards
-        label_lines_count = len(self.options.get('label', '').split('\n'))
-        if label_lines_count:
-            offset = label_lines_count * builder.font_size
-            y1 += offset
-            y2 += offset
+    def draw_arrow(self, root: ET.Element, x1, x2, y1, y2, arrow_id, color):
         # Params (:>)
         y_delta = 2
         if self.src == self.dst:
             # Special case: curved arc
-            if builder.parser.context['arcgradient'] < 10:
-                y2 += 10
             arc_magnitude = 100
             ET.SubElement(root, 'path', {
                 **self.options,
@@ -213,13 +130,29 @@ class Arc(Arity2):
                 'marker-end': f"url(#{arrow_id})",
                 'stroke-dasharray': '5, 3' if self.element == '>>' else '',
             })
+
+    def draw(self, builder, root: ET.Element):
+        # Arc color
+        color = self.options.get('linecolour') or self.options.get('linecolor') or "black"
+        arrow_id = self.get_arrow_tip_id(color)
+        # Arc coordinates
+        offset = utils.get_offset_from_label_multiple_lines(self.options.get('label', ''), builder.font_size)
+        x1 = builder.participants_coordinates[self.src]
+        y1 = builder.current_height + builder.margin + offset
+        x2 = builder.participants_coordinates[self.dst]
+        y2 = y1 + builder.parser.context['arcgradient']
+        if self.src == self.dst and builder.parser.context['arcgradient'] < 10:
+            # avoid having a squashed arc to self
+            y2 += 10
+
         # Arrow (see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/marker-end)
+        self.draw_arrow(root, x1, x2, y1, y2, arrow_id, color)
         self.draw_arrow_tip(root, arrow_id, color)
         # Lifelines of participants
         utils.expand_lifelines(builder, root, y1=builder.current_height, y2=y2, extra_options=self.options)
         builder.current_height = y2
         # Label (last element drawn since the rendering order is based on the document order)
-        self.draw_label(root, x1, x2, y1, y2, builder.font_size)
+        utils.draw_label(root, x1, x2, y1, y2, builder.font_size, self.options, arc_to_self=self.src == self.dst)
 
 
 class Box(Arity2):
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,29 @@
 import xml.etree.ElementTree as ET
 import random
 
+# TODO generify usage of font
+HELVETICA = {
+    'capheight': 718,
+    'xheight': 523,
+    'ascender': 718,
+    'descender': -207,
+    'unicode_to_width': {
+        32: 278, 33: 278, 34: 355, 35: 556, 36: 556, 37: 889, 38: 667, 39: 222, 40: 333, 41: 333, 42: 389, 43: 584,
+        44: 278, 45: 333, 46: 278, 47: 278, 48: 556, 49: 556, 50: 556, 51: 556, 52: 556, 53: 556, 54: 556, 55: 556,
+        56: 556, 57: 556, 58: 278, 59: 278, 60: 584, 61: 584, 62: 584, 63: 556, 64: 1015, 65: 667, 66: 667, 67: 722,
+        68: 722, 69: 667, 70: 611, 71: 778, 72: 722, 73: 278, 74: 500, 75: 667, 76: 556, 77: 833, 78: 722, 79: 778,
+        80: 667, 81: 778, 82: 722, 83: 667, 84: 611, 85: 722, 86: 667, 87: 944, 88: 667, 89: 667, 90: 611, 91: 278,
+        92: 278, 93: 278, 94: 469, 95: 556, 96: 222, 97: 556, 98: 556, 99: 500, 100: 556, 101: 556, 102: 278, 103: 556,
+        104: 556, 105: 222, 106: 222, 107: 500, 108: 222, 109: 833, 110: 556, 111: 556, 112: 556, 113: 556, 114: 333,
+        115: 500, 116: 278, 117: 556, 118: 500, 119: 722, 120: 500, 121: 500, 122: 500, 123: 334, 124: 260, 125: 334,
+        126: 584, 161: 333, 162: 556, 163: 556, 164: 167, 165: 556, 166: 556, 167: 556, 168: 556, 169: 191, 170: 333,
+        171: 556, 172: 333, 173: 333, 174: 500, 175: 500, 177: 556, 178: 556, 179: 556, 180: 278, 182: 537, 183: 350,
+        184: 222, 185: 333, 186: 333, 187: 556, 188: 1000, 189: 1000, 191: 611, 193: 333, 194: 333, 195: 333, 196: 333,
+        197: 333, 198: 333, 199: 333, 200: 333, 202: 333, 203: 333, 205: 333, 206: 333, 207: 333, 208: 1000, 225: 1000,
+        227: 370, 232: 556, 233: 778, 234: 1000, 235: 365, 241: 889, 245: 278, 248: 222, 249: 611, 250: 944, 251: 611,
+    }
+}
+
 
 def random_color():
     """ Generates a random hex color """
@@ -20,3 +43,57 @@ def expand_lifelines(builder, root: ET.Element, y1, y2, extra_options: dict):
             'y2': str(y2),
             **extra_options,
         })
+
+
+def get_offset_from_label_multiple_lines(label, font_size):
+    """ A label may have multiple lines, hence needing the arcs to be shifted downwards. """
+    offset = 0
+    label_lines_count = len(label.split('\n'))
+    if label_lines_count:
+        offset = label_lines_count * font_size
+    return offset
+
+
+def draw_label(root, x1, x2, y1, y2, font_size, options, arc_to_self=False):
+    label = options.get('label')
+    if not label:
+        return
+    MARGIN = 1  # margin before and after the label
+    OFFSET = HELVETICA['ascender']/2 * font_size/1000  # offset above the arc
+    g = ET.Element('g')
+    lines_count = len(label.split('\n'))
+    for idx, lab in enumerate(label.split('\n')):  # labels may contain newline character
+        # Draw Boxes
+        text_width = sum(HELVETICA['unicode_to_width'].get(ord(c), 0) for c in lab) * font_size / 1000
+        if arc_to_self:
+            x = x1 + OFFSET
+            y = (y1 - OFFSET)
+        else:
+            x = min(x1, x2) + abs(x2 - x1) / 2 - text_width / 2
+            y = ((y1 + y2) / 2 - OFFSET)
+        text_height = (HELVETICA['ascender'] - HELVETICA['descender']) * font_size / 1000
+        scaled_ascender = HELVETICA['ascender'] * font_size / 1000
+        y -= (lines_count - idx - 1) * text_height
+        box_x = x - MARGIN
+        if lab == '':
+            continue
+        rect = ET.SubElement(g, 'rect', {
+            'x': str(box_x),
+            'y': str(y - scaled_ascender),
+            'width': str(text_width + 2 * MARGIN),
+            'height': str(text_height),
+            'fill': options.get('textbgcolour') or options.get('textbgcolor') or 'white',
+        })
+        # Sometimes, the text goes beyond the SVG frame
+        max_x = float(rect.attrib['x']) + float(rect.attrib['width'])
+        if max_x > float(root.attrib['width']):
+            root.attrib['width'] = str(max_x)
+        # Draw text inside the box
+        text = ET.SubElement(g, 'text', {
+            'x': str(box_x),
+            'y': str(y),
+            'style': f"font-size: {font_size}",
+            'font-family': 'Helvetica',
+        })
+        text.text = lab
+    root.append(g)
